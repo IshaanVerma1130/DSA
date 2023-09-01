74f03ae6b276affb8e4a3e2797877db3773089f0
IshaanVerma1130 <ishaanverma100@gmail.com>
2023-09-01 22:12:19 +0930

    Palindromic Questions

---
+++ DP/LongestCommonSubsequence/Problems/MinInsertionAndDeletion.py
@@ -0,0 +1,22 @@
+def Solve(x: str, y: str, n: int, m: int) -> int:
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+
+    for i in range(n + 1):
+        for j in range(m + 1):
+            if i == 0 or j == 0:
+                dp[i][j] = 0
+
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if x[i - 1] == y[j - 1]:
+                dp[i][j] = 1 + dp[i - 1][j - 1]
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+    return (n - dp[n][m]) + (m - dp[n][m])
+
+
+str1 = "heap"
+str2 = "pea"
+
+print(Solve(str1, str2, len(str1), len(str2)))
